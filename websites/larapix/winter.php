e1bdaee04752fde6aea34dec72b60f032b5fe72d
Kian Moretz <meecoder@users.noreply.github.com>
2014-01-20 08:03:55 -0500

    fix text in winter story larapix

--- websites/larapix/winter.php
+++ websites/larapix/winter.php
@@ -19,35 +19,35 @@ Look for one penguin, a mommy seal and her baby, three snowy bunnies,
 and three white doves in this story.
 </p>
 <p>
-<img src="Images/winter1.jpg"></img>
+<img src="Images/winter1.jpg"></img></br>
 "Aah! I'm a huge animal!", says Mammy the mammoth.
 "I'm going to get you!", said Sabry the sabertoothed tiger.
 Polie the polar bear just sat there and watched.
-<img src="Images/winter2.jpg"></img>
+<img src="Images/winter2.jpg"></img></br>
 Look! Mammy had a baby! It's so cute!
 Sabry is keeping an eye on them. Polie left because he
  thought he was allergic to babies.
-<img src="Images/winter3.jpg"></img>
+<img src="Images/winter3.jpg"></img></br>
 Look! Polie came back! Yay!! Two babies are playing together, 
 but Polie is staying far away from them. Look! There's Sabry! AAH!!
-<img src="Images/winter4.jpg"></img>
+<img src="Images/winter4.jpg"></img></br>
 Whoopsie! Polie had 2 babies! AAH!! He started to run. 
 Oh no! Sabry is chasing the mammoths! "Help!", said the baby!
-<img src="Images/winter5.jpg"></img>
+<img src="Images/winter5.jpg"></img></br>
 At least they got away from Sabry. Finally they are safe!
 It is nighttime, so they shuld watch out.
-<img src="Images/winter6.jpg"></img>
+<img src="Images/winter6.jpg"></img></br>
 Aah! Sabry is back! The playing babies need to watch out!
-<img src="Images/winter7.jpg"></img>
+<img src="Images/winter7.jpg"></img></br>
 Look! Polie and three of her friends' babies are here! Oh no! Sabry 
 bit the baby mammoth!
-<img src="Images/winter8.jpg"></img>
+<img src="Images/winter8.jpg"></img></br>
 Oh! I never knew Polie had twins! One of the friend's babies is 
 still there, too.
-<img src="Images/winter9.jpg"></img>
+<img src="Images/winter9.jpg"></img></br>
 Polie is ready to hibernate, so he ate a lot and got way bigger. 
 And yay, the mammoths are getting away! But it's not over yet.
-<img src="Images/winter10.jpg"></img>
+<img src="Images/winter10.jpg"></img></br>
 The end.
 </p>
 </body>
