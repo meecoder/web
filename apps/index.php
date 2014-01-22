65ebd11e8734012fd67cce1e39850dff7535003c
Kian Moretz <meecoder@users.noreply.github.com>
2014-01-22 05:31:49 -0500

    Make apps index.php

--- apps/index.php
+++ apps/index.php
@@ -1,3 +1,20 @@
 <?php
 include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 ?>
+<html>
+<head>
+<title>
+kCompute Apps
+</title>
+</head>
+<body bgcolor="8B0000">
+<h1>
+kCompute Apps
+</h1>
+<p>
+Go to the pages below to download and for information on the different apps.
+<a href="mathpy">mathpy</a>
+<a href="dummyphone">DummyPhone App</a>
+</p>
+</body>
+</html>
