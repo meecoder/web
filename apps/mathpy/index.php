477a80198069cf21c7457418b0632b62ac7b4dba
Kian Moretz <meecoder@users.noreply.github.com>
2014-01-22 05:36:38 -0500

    work on mathpy site

--- apps/mathpy/index.php
+++ apps/mathpy/index.php
@@ -3,7 +3,7 @@
 include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 ?>
 <title>
-mathpy - kApps
+mathpy - kCompute Apps
 </title>
 </head>
 <body bgcolor="ASDF00">
@@ -11,6 +11,8 @@ mathpy - kApps
 mathpy app
 </h1>
 <p>
+This is a python app that gives you a math question for you to answer. You can choose the type. If your answer is incorrect the real answer will be shown.
+The code can be downloaded <a href="mathpy.zip">here</a> and run with a Python 3 interpreter.
 </p>
 </body>
 </html>
