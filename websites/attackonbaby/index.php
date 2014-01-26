5032a4c16fd11f779cc90cc4566f195003779ecc
Kian Moretz <meecoder@users.noreply.github.com>
2014-01-26 08:00:22 -0500

    change attackonbaby html links to php, fix spacing

--- websites/attackonbaby/index.php
+++ websites/attackonbaby/index.php
@@ -10,10 +10,10 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <p style="color:green">Babies are evil.</p>
 <h2>URGENT! BABY NEWS! READ CHAPTER 1 OF FOUND by MARGARET PETERSON HADDIX for DETAILS!</h2>
 <p>
-<a href="info.html">About   </a>  
-<a href="weapons.html">Weapons   </a>  
-<a href="contact.html">Contact Us   </a>  
-<a href="map.html">A Map of Baby Land   </a>  
+<a href="info.php">About   </a>
+<a href="weapons.php">Weapons   </a>
+<a href="contact.php">Contact Us   </a>
+<a href="map.php">A Map of Baby Land   </a>
 </p>
 </body>
 </html>
--- websites/attackonbaby/map.php
+++ websites/attackonbaby/map.php
@@ -5,7 +5,7 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <title>Attack On Baby</title>
 </head>
 <body bgcolor="AA2A2A">
-<a href="index.html"><img width="160" height="120" src="logo.png"></img></a>
+<a href="index.php"><img width="160" height="120" src="logo.png"></img></a>
 <h1 style="color:red"="AA2AAA">Attack On Baby</h1>
 <p style="color:green">Babies are evil.</p>
 <h3>This is a map of Babyland, the land where the babies meet and store weapons. </h3>
