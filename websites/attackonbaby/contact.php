8658c1e11044257cef157e59d4b94e95790bb60b
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-17 19:03:49 -0400

    update attack on baby contact info

--- websites/attackonbaby/contact.php
+++ websites/attackonbaby/contact.php
@@ -8,5 +8,6 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <a href="index.php"><img width="160" height="120" src="logo.png"></img></a>
 <h1 style="color:red"="AA2AAA">Attack On Baby</h1>
 <p style="color:green">Babies are evil.</p>
-<h3>The location of Attack on Baby headquarters is secret. To contact us please contact Kian Moretz or Jack Fuller. </h3>
+<h3>The location of Attack on Baby headquarters is secret. For more information please contact Kian Moretz or Jack Fuller. </h3>
+<p>All Attack on Baby agents will recieve information about how to find the headquarters and enter it.</p>
 </html>
