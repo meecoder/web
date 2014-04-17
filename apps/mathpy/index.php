05a67bb0a5d398786645a6d987b550049e9bc14e
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-17 18:57:02 -0400

    add more text to mathpy page

--- apps/mathpy/index.php
+++ apps/mathpy/index.php
@@ -11,7 +11,8 @@ mathpy - kCompute Apps
 mathpy app
 </h1>
 <p>
-This is a python app that gives you a math question for you to answer. You can choose the type. If your answer is incorrect the real answer will be shown.
+This is a python app that gives you a math question for you to answer. You can choose the type of problem - addition, subtraction, multiplication, division, or exponents. If your answer is incorrect, the correct answer will be shown.
+This is currently still in development - we are adding multiple problems at a time, percent accuracy, and a correct and incorrect count at the end.
 The code can be downloaded <a href="mathpy.zip">here</a> and run with a Python 3 interpreter.
 </p>
 </body>
