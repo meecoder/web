737c9e84f7e207f269a07b51f87b1d48cba598c3
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-29 19:05:44 -0400

    add comments to mcmk

--- websites/mcmk/smellet/index.php
+++ websites/mcmk/smellet/index.php
@@ -28,5 +28,10 @@ Grape, Cinnamon-Raspberry, Buttercream, Mint, and many others.
 <a href="moisturizer.php">Moisturizer</a>
 <a href="mini.php">Mini</a>
 <a href="mama.php">Mama</a>
+<!-- begin htmlcommentbox.com -->
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+ <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+ <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=(hcb_user.PAGE || ""+window.location), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%247.bGleVasiPPOiHF49trb0"+"&opts=342&num=10");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+<!-- end htmlcommentbox.com -->
 </body>
 </html>
