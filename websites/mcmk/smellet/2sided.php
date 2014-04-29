645cd92ef6b42808d0d6225d1844c09ec93a4d21
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-29 19:05:29 -0400

    add comments to mcmk

--- websites/mcmk/smellet/2sided.php
+++ websites/mcmk/smellet/2sided.php
@@ -11,5 +11,10 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <p>
 The 2-Sided Smellet is almost exactly like a regular Smellet - except that it has 2 sides. Each side has a different scent. That way, you have 2 scents in 1! The most popular 2-Sided Smellet has been the grape and cinnamon-raspberry.
 </p>
+<!-- begin htmlcommentbox.com -->
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+ <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+ <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=(hcb_user.PAGE || ""+window.location), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%247.bGleVasiPPOiHF49trb0"+"&opts=342&num=10");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+<!-- end htmlcommentbox.com -->
 </body>
 </html>
