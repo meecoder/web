f6ead63660895f6675966b84166047773851e182
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-29 18:44:11 -0400

    mcmk html to php links fix

--- websites/mcmk/index.php
+++ websites/mcmk/index.php
@@ -6,9 +6,9 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <title> MCMK Inventions </title>
 </head>
 <body bgcolor="1F93FF">
-<A HREF="index.html"><img src="mcmk.png" height="120" width="160"></A> </br>
+<A HREF="index.php"><img src="mcmk.png" height="120" width="160"></A> </br>
 <h1> Hello. Welcome to MCMK Inventions. </h1>
-<a href="products.html">Products</a>
-<a href="announcements.html">Announcements</a>
+<a href="products.php">Products</a>
+<a href="announcements.php">Announcements</a>
 </BODY>
 </html>
