a15bf0399eb22a72f4a40a1953cfef87fda38c00
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-29 18:49:03 -0400

    mcmk html to php links fix

--- websites/mcmk/smellet/moisturizer.php
+++ websites/mcmk/smellet/moisturizer.php
@@ -6,7 +6,7 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <title>MCMK Smellet--Moisturizer</title>
 </head>
 <body bgcolor="CD8852">
-<A HREF="../index.html"><img src="../mcmk.png" height="120" width="160"></A>
+<A HREF="../index.php"><img src="../mcmk.png" height="120" width="160"></A>
 <h1>The Smellet Moisturizer</h1>
 <p>
 The Smellet Moisturizer is very different from an ordinary Smellet. It is usually wetter and loosely wrapped. They are also not as strongly scented, and may be thicker as well as wtter. They were invented by Molly Smith.
