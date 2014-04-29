d9711269e6adfff782e77a8695c14730235832d6
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-29 18:49:48 -0400

    mcmk html to php links fix

--- websites/mcmk/smellet/mama.php
+++ websites/mcmk/smellet/mama.php
@@ -6,7 +6,7 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <title>MCMK Smellet--Mama</title>
 </head>
 <body bgcolor="CD8852">
-<A HREF="../index.html"><img src="../mcmk.png" height="120" width="160"></A>
+<A HREF="../index.mcmk"><img src="../mcmk.png" height="120" width="160"></A>
 <h1>The Smellet Mama</h1>
 <p>
 The Smellet Mama is a large Smellet usually not wrapped as much or as tightly. They were created by Molly Smith.
