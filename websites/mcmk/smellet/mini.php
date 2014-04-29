024f1750a8e2784acd815ec07a15523576262548
Kian Moretz <meecoder@users.noreply.github.com>
2014-04-29 19:06:01 -0400

    add comments to mcmk

--- websites/mcmk/smellet/mini.php
+++ websites/mcmk/smellet/mini.php
@@ -11,5 +11,10 @@ include($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 <p>
 The Mini Smellet is a smaller and easier to carry version of a Smellet. They are half to 3/4 the size of an ordinary Smellet. They are created by Claire Findlay.
 </p>
+<!-- begin htmlcommentbox.com -->
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+ <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+ <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=(hcb_user.PAGE || ""+window.location), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%247.bGleVasiPPOiHF49trb0"+"&opts=342&num=10");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+<!-- end htmlcommentbox.com -->
 </body>
 </html>
